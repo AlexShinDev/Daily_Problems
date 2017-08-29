ba06c4e3442adc3f61f0b9b62b200498e271c5f6
xthegoodguy <xashinx@gmail.com>
2017-08-29 17:38:45 -0500

    tuesday daily challenge

---
+++ week_2/tuesday.rb
@@ -0,0 +1,12 @@
+movies = [
+  {id: 1, title: "Die Hard", rating: 4.0},
+  {id: 2, title: "Bad Boys", rating: 5.0},
+  {id: 3, title: "The Chamber", rating: 3.0},
+  {id: 4, title: "Fracture", rating: 2.0}
+]
+
+p movies_1 = movies.select{ |movie| movie[:title].downcase.include?("b") }
+
+movies_1.map!{ |movie| movie[:id] }
+
+p movies_1
